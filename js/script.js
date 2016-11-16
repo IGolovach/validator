5a306070e1f1cd6f6abc444dc001d2ce4e6c2463
Irina Golovach <irene.golovach@gmail.com>
2016-11-16 02:04:52 +0200

    Block to check fields onchange

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+$("form>div").addClass("form-group input-group col-sm-offset-2 col-xs-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-8 col-sm-8 col-md-8 col-lg-8");
+$("span[aria-hidden='true']").addClass("glyphicon form-control-feedback");
+
 $(function(){
 	$('#signUpForm').validator({
 		feedback: {
@@ -6,23 +9,42 @@ $(function(){
 		}
 	});
 });
-
-$("form>div").addClass("form-group input-group col-sm-offset-2 col-xs-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-8 col-sm-8 col-md-8 col-lg-8");
-
+/*
+//
+//(in progress)Code to check fields
+//
+flag = 0;
+function button(){	
+    if(messageStat == 0){	
+        $("#submit").hide("slow");
+		if(!flag)
+		{flag = 1;
+		$('#result').append('<p color="red">* You need to fill all the fields</p>');}					 	
+    }
+}
+$("input").change(function(){
+	checkedCheckbox = $("#agree").prop("checked");
+	if( name.length == 1 && secondname.length == 1 && pass.length == 1 && email.length == 1 && gender == 1){
+		if(checkedCheckbox){
+		$("#submit").show("slow");}
+	}
+	if(name.length == 0 || secondname.length == 0 || pass.length == 0 || email.length == 0 || gender == 0){ 
+            messageStat = 0;  button();              
+    }
+});
+*/
 function get_result() {
  	var msg = $('#signUpForm').serialize();	
 	/*var name= $('input[name="name"]').val();   
 	var secondname= $('input[name="secondname"]').val();
 	var email= $('input[name="email"]').val();;  
 	var pass = $('input[name="pass"]').val();
-	var gender = $('select[name="gender"]').val();*/
-	
+	var gender = $('select[name="gender"]').val();*/	
         $.ajax({
           type: 'POST',
           url: 'http://codeit.pro/frontTestTask/user/registration',
-          data: msg,/*здесь возможна неверная передача данных*/          
+          data: msg,         
 		  success: function(data) {
-			alert('Data has been recieved');
 			alert(data.message);
 			$.getJSON('http://codeit.pro/frontTestTask/user/registration', function(data) {
 				//$('#result').append('<p>' + data.message +'; ' + data.status + '</p>');					
@@ -32,5 +54,4 @@ function get_result() {
 			alert('Возникла ошибка');
           }
         });
-
 }
